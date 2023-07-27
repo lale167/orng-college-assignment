5585eee724d477968105f80254b407d50804446b
lale167 <108874007+lale167@users.noreply.github.com>
2023-07-27 17:57:52 +0200

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const burger = document.querySelector(".burger");
+const menuItems = document.querySelector(".menu-items")
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
+    menuItems.classList.toggle("active");
 })
