9c84037b5b38e5a50dcc4624cc8545c9aac714f7
lale167 <108874007+lale167@users.noreply.github.com>
2023-07-31 18:37:47 +0200

    nzm koji

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const burger = document.querySelector(".burger");
 const menuItems = document.querySelector(".menu-items")
 
+const menuContact = document.querySelector(".menu-contact")
+const scrollToContact = document.querySelector(".contact")
+
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     menuItems.classList.toggle("active");
+})
+
+menuItems.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    menuItems.classList.toggle("active");
+})
+
+menuContact.addEventListener("click", () => {
+    scrollToContact.scrollIntoView()
 })
