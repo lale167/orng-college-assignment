8b2b25ca941c734784e75a44a0f870f414a858b0
lale167 <108874007+lale167@users.noreply.github.com>
2023-08-16 20:43:56 +0200

    pre-final

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const scrollToContact = document.querySelector(".contact")
 const menuProducts = document.querySelector(".menu-products")
 const scrollToProducts = document.querySelector(".products")
 
+const footerAboutUs = document.querySelector(".footer-about-us")
+const footerTechnology = document.querySelector(".footer-technology")
+
 // burger popup
 
 
@@ -32,24 +35,26 @@ menuItems.addEventListener("click", () => {
 
 menuAboutUs.addEventListener("click", () => {
     scrollToAboutUs.scrollIntoView();
-    window.scrollBy(0,-60);
+})
+
+footerAboutUs.addEventListener("click", () => {
+    scrollToAboutUs.scrollIntoView();
 })
 
 menuTechnology.addEventListener("click", () => {
     scrollToTechnology.scrollIntoView();
-    window.scrollBy(0,-60);
+})
+
+footerTechnology.addEventListener("click", () => {
+    scrollToTechnology.scrollIntoView();
 })
 
 menuContact.addEventListener("click", () => {
     scrollToContact.scrollIntoView();
-    if(window.innerHeight<750){
-        window.scrollBy(0,-40);
-    }
 })
 
 menuProducts.addEventListener("click", () => {
     scrollToProducts.scrollIntoView();
-        window.scrollBy(0,-60);
 })
 
 // img popup
@@ -58,6 +63,13 @@ menuProducts.addEventListener("click", () => {
 const popup = document.querySelector('.popup')
 const span = document.querySelector('.popup span')
 const popupImg = document.querySelector('.popup img')
+const mask = document.querySelector('.popup-mask')
+const popupContainer = document.querySelector('.popup-container')
+
+const popupInfo1 = document.querySelector('.popup-info1')
+const popupInfo2 = document.querySelector('.popup-info2')
+const popupInfo3 = document.querySelector('.popup-info3')
+const popupInfo4 = document.querySelector('.popup-info4')
 
 const button1 = document.querySelector('.button1')
 const button2 = document.querySelector('.button2')
@@ -65,33 +77,47 @@ const button3 = document.querySelector('.button3')
 const button4 = document.querySelector('.button4')
 
 
-
-
 button1.onclick = () => {
-    popup.style.display = 'inline-block';
+    popup.style.display = "inline-block";
     popupImg.setAttribute('src', 'images/img1.png');
+    popupInfo1.style.display = 'inline-block'
+    popupInfo2.style.display = 'none'
+    popupInfo3.style.display = 'none'
+    popupInfo4.style.display = 'none'
 }
 
 button2.onclick = () => {
     popup.style.display = 'inline-block';
     popupImg.setAttribute('src', 'images/img2.png')
+    popupInfo1.style.display = 'none'
+    popupInfo2.style.display = 'inline-block'
+    popupInfo3.style.display = 'none'
+    popupInfo4.style.display = 'none'
 }
 
 button3.onclick = () => {
     popup.style.display = 'inline-block';
     popupImg.setAttribute('src', 'images/img3.jpg')
+    popupInfo1.style.display = 'none'
+    popupInfo2.style.display = 'none'
+    popupInfo3.style.display = 'inline-block'
+    popupInfo4.style.display = 'none'
 }
 
 button4.onclick = () => {
     popup.style.display = 'inline-block';
     popupImg.setAttribute('src', 'images/img4.jpg')
+    popupInfo1.style.display = 'none'
+    popupInfo2.style.display = 'none'
+    popupInfo3.style.display = 'none'
+    popupInfo4.style.display = 'inline-block'
 }
 
 
-
-
-
-
 span.onclick = () => {
+    popup.style.display = 'none';
+}
+
+mask.onclick = () => {
     popup.style.display = 'none';
 }
